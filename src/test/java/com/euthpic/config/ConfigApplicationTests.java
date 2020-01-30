96529d2bac53c66107d4658336f8e1412b1bde2f
Euthpic <490114771@qq.com>
2020-01-30 15:26:54 +0800

    initial
    Signed-off-by: Euthpic <490114771@qq.com>

---
+++ src/test/java/com/euthpic/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euthpic.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
